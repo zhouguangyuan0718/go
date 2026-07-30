ff6a6fbd88d9d028eb92fe24504f8736206ebe61
ZhouGuangyuan <zhouguangyuan.xian@gmail.com>
2026-07-31 02:37:13 +0800

    cmd/compile: preserve pointer-free data sections for LLVM

---
+++ test/codegen/llvm_data_sections.go
@@ -0,0 +1,25 @@
+// asmcheck
+
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package codegen
+
+// LLVM-DAG: @codegen.llvmNoPtrBSS = global <{ [8 x i8] }> zeroinitializer, section ".noptrbss"
+// LLVM-DAG: @codegen.llvmNoPtrData = global <{ [8 x i8] }> <{ [8 x i8] c"*\00\00\00\00\00\00\00" }>, section ".noptrdata"
+var llvmNoPtrBSS int
+var llvmNoPtrData = 42
+
+type llvmPointerData struct {
+	p *int
+}
+
+// LLVM-DAG: @codegen.llvmPointerBSS = global <{ [8 x i8] }> zeroinitializer, section ".bss"
+// LLVM-DAG: @codegen.llvmInitializedPointerData = global <{ ptr }> <{ ptr @codegen.llvmNoPtrData }>, section ".data"
+var llvmPointerBSS llvmPointerData
+var llvmInitializedPointerData = llvmPointerData{p: &llvmNoPtrData}
+
+func llvmUseDataSections() (*int, int, int, *int) {
+	return llvmPointerBSS.p, llvmNoPtrBSS, llvmNoPtrData, llvmInitializedPointerData.p
+}
