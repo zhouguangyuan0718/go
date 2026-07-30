543915df20695fecc8812c1c7f11feb1ea8c98d2
zhouguangyuan0718 <zhouguangyuan.xian@gmail.com>
2026-07-30 23:44:19 +0800

    cmd/llvmplugin: propagate indirect statepoint calls (#18)

---
+++ src/cmd/llvmtoolexec/testdata/indirect/main.go
@@ -0,0 +1,19 @@
+// Copyright 2026 The GoALLC Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+//go:noinline
+//go:nosplit
+func invoke(fn func(*int), value *int) {
+	fn(value)
+}
+
+//go:noinline
+func target(*int) {}
+
+func main() {
+	var value int
+	invoke(target, &value)
+}
