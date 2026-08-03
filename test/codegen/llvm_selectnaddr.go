256f80e00c440550266607fc37af193123a851f2
zhouguangyuan0718 <zhouguangyuan.xian@gmail.com>
2026-08-03 16:42:46 +0800

    cmd/compile: lower SelectNAddr to LLVM result homes (#58)

---
+++ test/codegen/llvm_selectnaddr.go
@@ -0,0 +1,30 @@
+// asmcheck
+
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package codegen
+
+type llvmAddressedCallResult [20]int
+
+// LLVM-LABEL: define goabiinternal i64 @codegen.llvmReadAddressedCallResult(
+// LLVM: [[HOME:%[^ ]+\.home]] = alloca [20 x i64], align 8
+// LLVM: [[RESULT:%.*]] = call goabiinternal [20 x i64] @codegen.llvmMakeAddressedCallResult(i64 %seed)
+// LLVM-NEXT: store [20 x i64] [[RESULT]], ptr [[HOME]], align 8
+// LLVM: load i64, ptr
+// LLVM: ret i64
+//
+//go:noinline
+func llvmReadAddressedCallResult(seed int) int {
+	result := llvmMakeAddressedCallResult(seed)
+	return result[0] + result[len(result)-1]
+}
+
+//go:noinline
+func llvmMakeAddressedCallResult(seed int) (result llvmAddressedCallResult) {
+	for i := range result {
+		result[i] = seed + i
+	}
+	return
+}
