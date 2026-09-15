2294d82af460786daed9d5a2f21c3407a6b8af2e
ZhouGuangyuan <zhouguangyuan.xian@gmail.com>
2026-09-16 00:06:25 +0800

    test/codegen: check GC-leaf attributes on runtime declarations

--- test/codegen/llvm_memops.go
+++ test/codegen/llvm_memops.go
@@ -76,26 +76,26 @@ func llvmMovePointerToStack(src *llvmPointerStackZero) *int {
 
 // LLVM-DAG: define goabiinternal void @codegen.llvmMoveOverlapSized(
 // LLVM-ARM64-DAG: call void @llvm.memmove.p0.p0.i64(ptr align 1 %dst, ptr align 1 %src, i64 32, i1 false)
-// LLVM-AMD64-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr %src, i64 32) #{{[0-9]+}}
+// LLVM-AMD64-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr %src, i64 32){{$|,}}
 func llvmMoveOverlapSized(dst, src *[32]byte) {
 	*dst = *src
 }
 
 // LLVM-DAG: define goabiinternal void @codegen.llvmMoveAligned(
 // LLVM-ARM64-DAG: call void @llvm.memmove.p0.p0.i64(ptr align 8 %dst, ptr align 8 %src, i64 24, i1 false)
-// LLVM-AMD64-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr %src, i64 24) #{{[0-9]+}}
+// LLVM-AMD64-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr %src, i64 24){{$|,}}
 func llvmMoveAligned(dst, src *[3]uint64) {
 	*dst = *src
 }
 
 // LLVM-DAG: define goabiinternal void @codegen.llvmMoveLarge(
-// LLVM-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr {{%.*}}, i64 128) #{{[0-9]+}}
+// LLVM-DAG: call goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr %dst, ptr {{%.*}}, i64 128){{$|,}}
 func llvmMoveLarge(dst *[128]byte, src [128]byte) {
 	*dst = src
 }
 
 // LLVM-DAG: define goabiinternal i8 @codegen.llvmMemEq(
-// LLVM-DAG: call goabiinternal i8 @"runtime.memequal<builtin.{{[0-9]+}}>"(ptr {{%.*}}, ptr {{%.*}}, i64 {{%.*}}) #{{[0-9]+}}
+// LLVM-DAG: call goabiinternal i8 @"runtime.memequal<builtin.{{[0-9]+}}>"(ptr {{%.*}}, ptr {{%.*}}, i64 {{%.*}}){{$|,}}
 func llvmMemEq(a, b string) bool {
 	return a == b
 }
@@ -107,4 +107,7 @@ func llvmSlicemask(a []byte, i int) []byte {
 	return a[i:]
 }
 
-// LLVM-DAG: attributes #{{[0-9]+}} = { "gc-leaf-function" }
+// GC-leaf contracts belong to the runtime declarations, not individual calls.
+// LLVM-DAG: declare goabiinternal i8 @"runtime.memequal<builtin.{{[0-9]+}}>"(ptr, ptr, i64) #[[GC_LEAF:[0-9]+]]
+// LLVM-DAG: declare goabiinternal void @"runtime.memmove<builtin.{{[0-9]+}}>"(ptr, ptr, i64) #[[GC_LEAF]]
+// LLVM-DAG: attributes #[[GC_LEAF]] = { "gc-leaf-function" }
